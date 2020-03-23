823f07a0072b53c69a050eb1a526f7639cf7e294
Jonathan East <jonathan_east@ymail.com>
2020-03-23 03:26:08 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -6,8 +6,8 @@ function getIt() {
 }
 
 function frameIt() {
-  $('.tasty').on('load', function() {
-    $('img').css('border', 'red solid 1px');
+  $('img').on('load', function() {
+    $('img').addClass('.tasty').css('border', 'red solid 1px');
   });
 }
 
