b056d4b2561b0faf45e137ca6e9146e2ec07e17b
Jonathan East <jonathan_east@ymail.com>
2020-03-23 03:10:38 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,25 +1,23 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  });
+}
 
+function frameIt() {
+
+}
+
+function pressIt() {
+
+}
+
+function submitIt() {
+
+}
 $(document).ready(function(){
 
 // call functions here
-  function getIt() {
-    $('p').on('click', function() {
-      alert("Hey!");
-    });
-  }
-
-  function frameIt() {
-
-  }
-
-  function pressIt() {
-
-  }
-
-  function submitIt() {
-
-  }
-
 
 });
